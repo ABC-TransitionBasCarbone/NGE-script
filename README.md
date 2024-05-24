d77979aff2d05654631a0d6f10e16b490d93cfa2
Chloe M <chloe.molle@neuf.fr>
2024-05-24 10:48:38 +0200

    manage archi

---
+++ README.md
@@ -0,0 +1,5 @@
+SCRIPT NGE
+
+Deux possibilités pour le modèle :
+- Soit on récupère tout le modèle via le paquet npm (choix que j'ai fait pour l'instant dans index.js)
+- Soit, si on a besoin que de quelques règles et surtout si on a besoin de les retravailler/modifier, on peut les importer comme c'est montré dans data, puis les compiler avec compile-modele. 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import rules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-lang.fr.json' with { type: "json" }
 import Engine from 'publicodes'
-import { handleInputExcel, initOutputExcel } from './helper.js';
+import { handleInputExcel, initOutputExcel } from './helpers/utils.js';
+import { getSituationForTransport } from './helpers/transport.js';
 
 const { inputworksheet, numberOfRow } = await handleInputExcel();
 const { outputWorkbook, outputSheet } = await initOutputExcel();
@@ -10,10 +11,7 @@ for (let i = 3; i <= numberOfRow; i ++) {
     const currentRow = inputworksheet.getRow(i);
     
     engine.setSituation({
-        "transport . deux roues . empreinte au km": currentRow.getCell(4).value === "scooter thermique" ? "0.08" : "0.19",
-        "transport . deux roues . entretien pondéré": "45",
-        "transport . deux roues . km": currentRow.getCell(3).value,
-        "transport . deux roues . usager": currentRow.getCell(2).value
+        ...getSituationForTransport(currentRow.getCell(4).value, currentRow.getCell(3).value, currentRow.getCell(2).value)
     });
     
     const valeur = engine.evaluate('transport . deux roues . empreinte');
